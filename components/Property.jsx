d692765f7db4d8f59b2def108992119463a5836c
papa-react <makiya112545@gmail.com>
2021-12-11 20:16:53 -1200

    add millify to price

--- components/Property.jsx
+++ components/Property.jsx
@@ -1,4 +1,3 @@
-
 import Link from 'next/link';
 import Image from 'next/image';
 import { Box,  Flex, Badge, Text, Avatar } from "@chakra-ui/react";
@@ -11,13 +10,13 @@ const Property = ( { property:{ coverPhoto,coverVideo, price, rentFrequency, roo
         <Link href={`property/${externalID}`} passHref>
             <Flex  flexWrap='wrap' w='420px' p='5' pt='0' cursor='pointer'  >
                 <Box>
-                    <Image src={coverPhoto?.url} width={400} height={260} alt={title}  />
+                    <Image objectFit='cover'  src={coverPhoto?.url} width={400} height={260} alt="house"  />
                 </Box>
                 <Box w='full' >
                     <Flex items='center' pt='1' justify='space-between'  >
                         <Flex items='center'>
                             <Box pr='3' color='green.400'>{isVerified && <GoVerified color='green.400' />}</Box>
-                            <Text fontWeight='semibold' fontSize='sm'>birr {price}{rentFrequency && `/${rentFrequency}`}</Text>
+                            <Text fontWeight='semibold' fontSize='sm'>birr {millify(price)} {rentFrequency && `/${rentFrequency}`}</Text>
                         </Flex>
                         <Box>
                             <Avatar size='sm' src={agency?.logo?.url}></Avatar>
