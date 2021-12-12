266f932fe44847f93b456937f0f39ac5cd886047
papa-react <makiya112545@gmail.com>
2021-12-11 20:18:05 -1200

    add NProgress add wrrap componet withe Layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,31 @@
+import Router from 'next/router';
+import Head from 'next/head';
 import { ChakraProvider } from '@chakra-ui/react'
+import NProgress from 'nprogress';
+import "nprogress/nprogress.css";
+
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
+  // NProgress.configure({ showSpinner: false });
+  Router.events.on('routeChangeStart', () => {
+    NProgress.start();
+    NProgress.inc();
+  });
+
+  Router.events.on('routeChangeComplete', () => {
+    NProgress.done();
+    NProgress.remove();
+  })
+
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </>
   )
 }
 
