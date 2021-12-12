1fb2bb2121c40193e5d584eeed7501077b223abb
papa-react <makiya112545@gmail.com>
2021-12-12 00:22:45 -1200

    add formats to avif

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     images: {
-        domains:['bayut-production.s3.eu-central-1.amazonaws.com']
+        domains: ['bayut-production.s3.eu-central-1.amazonaws.com'],
+        formats: ['image/avif', 'image/webp'],
     }
 }
