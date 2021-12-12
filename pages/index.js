ecc0831e10fdbe1aa390a58530c7b40ed493264c
papa-react <makiya112545@gmail.com>
2021-12-12 07:49:26 -1200

    upercase

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ const Home = ({ propertiesForSale, propertiesForRent }) => {
         desc1=' Explore from Apartments, builder floors, villas'
         desc2='and more'
         buttonText='Explore Renting'
-        linkName='/search?purpose=for-rent'
+        linkName='/Search?purpose=for-rent'
         imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
       />
       <Flex flexWrap='wrap' w='full' mx='auto' >
@@ -28,7 +28,7 @@ const Home = ({ propertiesForSale, propertiesForRent }) => {
         desc1=' Explore from Apartments, land, builder floors,'
         desc2=' villas and more'
         buttonText='Explore Buying'
-        linkName='/search?purpose=for-sale'
+        linkName='/Search?purpose=for-sale'
         imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
       />
       <Flex flexWrap='wrap'>
