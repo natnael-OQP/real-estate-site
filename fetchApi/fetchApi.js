dbf3f59fec5f27505828f7285b1da0e46324f5de
papa-react <makiya112545@gmail.com>
2021-12-12 19:33:22 -1200

    add envirometnal varable

--- fetchApi/fetchApi.js
+++ fetchApi/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
     const {data} = await axios.get((url), {
         headers: {
             'x-rapidapi-host': 'bayut.p.rapidapi.com',
-            'x-rapidapi-key': '504fd0403dmsh222f9dd76bdd42fp1afcf7jsn525f2aeca63c'
+            'x-rapidapi-key': process.env.API_KEY
         }
     });
 
